5e66eeefb30c68cd53b48a50fe78f92093046ab8
Matthew Jorgensen <me@prplecake.com>
2023-05-21 01:17:53 -0500

    Initial Commit

---
+++ AC Remote/AC_RemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AC_RemoteApp.swift
+//  AC Remote
+//
+//  Created by prplecake on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct AC_RemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
